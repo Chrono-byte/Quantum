ae692c34a09efcee66d5dc4e57ba5958101b6241
Grant Sommer <grant@grantsommer.com>
2017-08-10 12:21:25 -0600

    forgot one command
    AAAA

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ io.on('connection', function(socket){
           msg.msg.match(/-([\S]*?)-/g).forEach(function(r){
             console.log("found: ",r);
             if(r){
-              r=r.replace("-","");
+              r=r.replace("-","").replace("-","");
               console.log("http://emoji.works/e/"+r);
               request("http://emoji.works/e/"+r,function(err,res,body){
                 console.log(res&&res.statusCode);
